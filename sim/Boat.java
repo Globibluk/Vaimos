fdbaac438cb98a0e6e4c632d5a80f7c0be0ab73c
Leitdorf <onegroni@free.fr>
2016-02-20 14:49:55 +0100

    Update Boat.java

--- sim/Boat.java
+++ sim/Boat.java
@@ -2,48 +2,70 @@ package sim;
 
 public class Boat {
 
-	private int posX;
-	private int posY;
+	private int iPosX;
+	private int iPosY;
+	private float fPosX;
+	private float fPosY;
 	private World world;
 	private Cell[][] grid;									// Cellule deja exploré
 	
 	public Boat(int posX, int posY, World world)
 	{
-		this.posX = posX;
-		this.posY = posY;
+		this.iPosX = posX;
+		this.fPosX = iPosX;
+		this.iPosY = posY;
+		this.fPosY = iPosY;
 		this.world = world;
 		world.setBoat(this);
 		grid = new Cell[world.getWorldHeight()][world.getWorldLength()];
 	}
 	
-	public void move(int posX, int posY)
+	public void move(float posX, float posY)
 	{
-		this.posX = posX;
-		this.posY = posY;
-		world.setBoat(this);
+		fPosX = posX;
+		if(fPosX >= world.getWorldLength() -1) fPosX = world.getWorldLength() -1;
+		if(fPosX < 0) fPosX = 0;
+		iPosX = Math.round(fPosX);
+		fPosY = posY;
+		if(fPosY >= world.getWorldHeight() -1) fPosY = world.getWorldHeight() -1;
+		if(fPosY < 0) fPosY = 0;
+		iPosY = Math.round(fPosY);
+		
+		System.out.println("" + iPosX + " " + iPosY + " " + " " + fPosX + " " + fPosY);
+
 	}
 	
-	public int getPosX()
+	public int getIPosX()
 	{
-		return posX;
+		return iPosX;
 	}
 	
-	public int getPosY()
+	public int getIPosY()
 	{
-		return posY;
+		return iPosY;
+	}
+	
+	public float getFPosX()
+	{
+		return fPosX;
+	}
+	
+	public float getFPosY()
+	{
+		return fPosY;
 	}
 	
 	public void analyseCell()
 	{
-		grid[posX][posY] = world.getCellXY(posX, posY);
+		grid[iPosX][iPosY] = world.getCellXY(iPosX, iPosY);
 		System.out.println(toStringCell());
-		world.getCellXY(posX, posY).setDecouvert();
+		world.getCellXY(iPosX, iPosY).setDecouvert();
 	}
 	
 	public String toStringCell()
 	{
-		Cell c = world.getCellXY(posX, posY);
-		return "Position : " + posX + "\t" + posY + "\n" +
+		Cell c = grid[iPosX][iPosY];
+		return "Position : " + fPosX + "\t" + fPosY + "\n" +
 				"Vent : " + c.getVent().getX() + "\t" + c.getVent().getY() + "\n" +
 				"Courant : " + c.getCourant().getX() + "\t" + c.getCourant().getY() + "\n" +
 				"Salinite : " + c.getSalinite() + "\n";
