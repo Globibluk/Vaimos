ce47c3abf3df487290ab041a5c257b049e65a533
Leitdorf <onegroni@free.fr>
2016-02-20 15:15:08 +0100

    Added files via upload

--- tools/Vector2D.java
+++ tools/Vector2D.java
@@ -2,7 +2,6 @@ package tools;
 
 public class Vector2D {
 
-	private static float randomRange = 2;
 	private float x;
 	private float y;
 	
@@ -14,8 +13,8 @@ public class Vector2D {
 
 	public Vector2D()
 	{
-		x = (float) Math.random() * randomRange - randomRange;
-		y = (float) Math.random() * randomRange - randomRange;
+		x = (float) Math.random() * 2 - 1;
+		y = (float) Math.random() * 2 - 1;
 	}
 
 	public float getX()
