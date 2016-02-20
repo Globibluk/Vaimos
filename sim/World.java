6a804b3bf4762748ab419dcc46611b580648da8f
Leitdorf <onegroni@free.fr>
2016-02-20 14:51:01 +0100

    Update World.java

--- sim/World.java
+++ sim/World.java
@@ -6,7 +6,7 @@ public class World {
 	private static int worldHeight;
 	private static int worldLength;
 	
-	private static Boat boat;
+	private Boat boat;
 	
 	public World(int height, int length)
 	{
@@ -23,6 +23,16 @@ public class World {
 			for(int j=0;j<length;j++)
 			{
 				grid[i][j] = new Cell();
+			}
+		}
+	}
+	
+	public void changementVent(){
+		for(int i=0;i<worldHeight;i++)
+		{
+			for(int j=0;j<worldLength;j++)
+			{
+				grid[i][j].setVent();
 			}
 		}
 	}
@@ -49,7 +59,7 @@ public class World {
 	
 	public void setBoat(Boat boat)
 	{
-		this.boat = boat;     										// Warnning ? 
+		this.boat = boat;
 	}
 	
 	public Boat getBoat()
