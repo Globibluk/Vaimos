f1fc30a6b03be6f0cd49b081c6329c92c4edba22
Leitdorf <onegroni@free.fr>
2016-04-25 19:24:44 +0200

    Update Cell.java

--- sim/Cell.java
+++ sim/Cell.java
@@ -7,7 +7,6 @@ public class Cell {
 	private Vector2D vent;
 	private Vector2D courant;
 	private float profondeur;
-	private static float profondeurMax = 1000;
 	private boolean decouvert;
 	private float salinite;											// Donnee en plus
 	
@@ -20,7 +19,7 @@ public class Cell {
 		salinite =(float) Math.random();
 	}
 	
-	public Cell()
+	public Cell(float profondeurMax)
 	{
 		vent = new Vector2D();
 		courant = new Vector2D();
@@ -64,5 +63,13 @@ public class Cell {
 
 	public void setDecouvert() {
 		this.decouvert = true;
+	}
+
+	public void setVent(Vector2D vent) {
+		this.vent = vent;
+	}
+
+	public void setCourant(Vector2D courant) {
+		this.courant = courant;
 	}
 }
