489514e518a202523650e4fee45a8238adea5867
Leitdorf <onegroni@free.fr>
2016-04-29 17:50:04 +0200

    Added files via upload

--- interpreter/Interpreter.java
+++ interpreter/Interpreter.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import boat.Boat;
 import sim.Simulation;
+import tools.Point;
 
 public class Interpreter extends Thread {
 	
@@ -12,12 +13,14 @@ public class Interpreter extends Thread {
 	
 	private int line;
 	private Boat boat;
+	private int delai;
 	
 	public Interpreter()
 	{
 		instructions = new ArrayList<Instruction>();
 		dataStructure =  new DataStructure();
 		line = 0;
+		delai = 0;
 	}
 	
 	public void addInstruction(Instruction i)
@@ -45,11 +48,11 @@ public class Interpreter extends Thread {
 		}
 		catch (NumberFormatException e)
 		{
-			System.out.println("Wrong variable at line " + line);
+			System.out.println("Wrong variable at line " + (line+1));
 		}
 		catch (IndexOutOfBoundsException e)
 		{
-			System.out.println("Wrong code at line " + line);
+			System.out.println("Wrong code at line " + (line+1));
 		} catch (Exception e) {}
 	}
 		
@@ -58,9 +61,11 @@ public class Interpreter extends Thread {
 	{
 		ArrayList<String> args;
 		ArrayList<Double> dList;
+		ArrayList<Point> pList;
 		Object result;
 		int index;
 		Double value0, value1;
+		Point pvalue;
 		String s;
 		Instruction i;
 		ArrayList<Integer> lastLoop = new ArrayList<Integer>();
@@ -68,7 +73,7 @@ public class Interpreter extends Thread {
 		for(line=0;line<instructions.size();line++)
 		{
 			try {
-				Thread.sleep(50);
+				Thread.sleep(delai);
 			} catch (InterruptedException e) {}
 			i = instructions.get(line);
 			args = i.getArgs();
@@ -91,28 +96,39 @@ public class Interpreter extends Thread {
 					break;
 				
 				case "set":
-
 					result = dataStructure.getValue(args.get(0));
 					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
 					dList = (ArrayList<Double>) result;
-					index = (int) getData(args.get(1));
+					value0 = (double) getData(args.get(1));
+					index = value0.intValue();
 					value0 = (double) getData(args.get(2));
 					
 					dList.set(index, value0);
 					break;
 					
 				case "get":
-					
 					result = dataStructure.getValue(args.get(0));
 					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
 					dList = (ArrayList<Double>) result;
-					index = (int) (getData(args.get(1)));
+					value0 = (double) getData(args.get(1));
+					index = value0.intValue();
 					s = args.get(2);
-					value1 = dList.get(index);
+					value0 = dList.get(index);
 					
-					dataStructure.setValue(s, value1);
+					dataStructure.setValue(s, value0);
 					break;
 					
+				case "index":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					dList = (ArrayList<Double>) result;
+					value0 = (double) getData(args.get(1));
+					s = args.get(2);
+					value1 = (double) dList.indexOf(value0);
+					
+					dataStructure.setValue(s, value1.intValue());
+					break;
+							
 				case "add":
 					result = dataStructure.getValue(args.get(0));
 					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
@@ -126,9 +142,90 @@ public class Interpreter extends Thread {
 					result = dataStructure.getValue(args.get(0));
 					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
 					dList = (ArrayList<Double>) result;
-					index = (int) getData(args.get(1));
+					value0 = (double) getData(args.get(1));
+					index = value0.intValue();
 					
 					dList.remove(index);
+					break;
+					
+				case "plist":
+					ArrayList<Point> plist = new ArrayList<Point>();
+					dataStructure.setValue(args.get(0), plist);
+					break;
+				
+				case "pset":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					pList = (ArrayList<Point>) result;
+					value0 = (double) getData(args.get(1));
+					index = value0.intValue();
+					value0 = (double) getData(args.get(2));
+					value1 = (double) getData(args.get(3));
+					
+					pvalue = new Point(value0.intValue(), value1.intValue());
+					
+					pList.set(index, pvalue);
+					break;
+					
+				case "pget":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					pList = (ArrayList<Point>) result;
+					value0 = (double) (getData(args.get(1)));
+					index = value0.intValue();
+					s = args.get(2);
+					pvalue = pList.get(index);
+					
+					dataStructure.setValue(s, pvalue);
+					break;
+					
+				case "pindex":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					dList = (ArrayList<Double>) result;
+					pvalue = (Point) getData(args.get(1));
+					s = args.get(2);
+					value1 = (double) dList.indexOf(pvalue);
+					
+					dataStructure.setValue(s, value1);
+					break;
+					
+				case "padd":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					pList = (ArrayList<Point>) result;
+					value0 = (double) getData(args.get(1));
+					value1 = (double) getData(args.get(2));
+					
+					pvalue = new Point(value0.intValue(), value1.intValue());
+					
+					pList.add(pvalue);
+					break;
+					
+				case "premove":
+					result = dataStructure.getValue(args.get(0));
+					if(!(result instanceof ArrayList)) throw new WrongVariableException(line);
+					pList = (ArrayList<Point>) result;
+					value0 = (double) getData(args.get(1));
+					index = value0.intValue();
+					
+					pList.remove(index);
+					break;
+					
+				case "px":
+					pvalue = (Point) dataStructure.getValue(args.get(0));
+					value0 = (double) pvalue.getX();
+					s = args.get(1);
+					
+					dataStructure.setValue(s, value0);
+					break;
+					
+				case "py":
+					pvalue = (Point) dataStructure.getValue(args.get(0));
+					value0 = (double) pvalue.getY();
+					s = args.get(1);
+					
+					dataStructure.setValue(s, value0);
 					break;
 					
 				case "+": 
@@ -215,19 +312,22 @@ public class Interpreter extends Thread {
 				case "print":
 				case "println":
 					s = "";
-					s += dataStructure.getValue(i.getArgs().get(0));
+					s += getData(i.getArgs().get(0));
 					if(i.getName() == "print") 
 						System.out.print(s);
 					else 
 						System.out.println(s);
 					break;
 					
+				case "comment":
+					break;
+					
 				case "posx":
-					dataStructure.setValue(i.getArgs().get(0), boat.getIPosX());
+					dataStructure.setValue(i.getArgs().get(0), boat.getFPosX());
 					break;
 					
 				case "posy":
-					dataStructure.setValue(i.getArgs().get(0), boat.getIPosY());
+					dataStructure.setValue(i.getArgs().get(0), boat.getFPosY());
 					break;
 					
 				case "depth":
@@ -243,15 +343,19 @@ public class Interpreter extends Thread {
 					break;
 				
 				case "setTarget":
-					int ax, ay, bx, by;
-					ax = (int) getData(args.get(0));
-					ay = (int) getData(args.get(1));
-					bx = (int) getData(args.get(2));
-					by = (int) getData(args.get(3));
+					double ax, ay, bx, by;
+					ax = (double) getData(args.get(0));
+					ay = (double) getData(args.get(1));
+					bx = (double) getData(args.get(2));
+					by = (double) getData(args.get(3));
 					
 					boat.setTarget(ax, ay, bx, by);
 					break;
-										
+				
+				case "delay":
+					delai = Integer.parseInt(i.getArgs().get(0));
+					break;
+				
 				default:
 					throw new UnknownInstructionException(line);
 					
@@ -356,17 +460,19 @@ public class Interpreter extends Thread {
 		return false;
 	}
 	
-	private double getData(String key) throws WrongVariableException
+	private Object getData(String key) throws WrongVariableException
 	{
-		double value;
+		Object obj = null;
 		try
 		{
-			value = Double.parseDouble(key);
+			obj = Double.parseDouble(key);
 		} catch (Exception e)
 		{
-			value = (double) dataStructure.getValue(key);
+			obj = dataStructure.getValue(key);
+			if(obj == null) throw new WrongVariableException(line);
+			return obj;
 		}
-		return value;
+		return obj;
 	}
 	
 }
